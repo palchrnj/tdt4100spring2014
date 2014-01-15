4fcf49a095f6fbfb792d54c20bebd2ba0c9bef39
thepaulus <paulus@Pal-Christians-MacBook-Pro.local>
2014-01-15 01:50:19 +0100

    added generic to stack

--- tdt4100/src/stateandbehavior/Stack.java
+++ tdt4100/src/stateandbehavior/Stack.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Stack {
 	
-	private ArrayList<String> stringList = new ArrayList<>();
+	private ArrayList<String> stringList = new ArrayList<String>();
 	
 	public void push(String str) {
 		stringList.add(str);
