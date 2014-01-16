9416439b10094631f48d1556380045271e37cf7f
thepaulus <paulus@Pal-Christians-MacBook-Pro.local>
2014-01-16 13:26:12 +0100

    updated tictactoe.java

--- tdt4100/src/objectstructures/TicTacToe.java
+++ tdt4100/src/objectstructures/TicTacToe.java
@@ -4,32 +4,42 @@ import java.util.Scanner;
 
 public class TicTacToe {
 	
-	// empty board position = " "
-	// player X = "X"
-	// player O = "O"
+	/*
+	 * PART 1
+	 */
+	
 	public String gridString;
+	public char player;
 	
 	public TicTacToe() {
 		gridString = "         ";
+		player = 'x';
 	}
 	
-	public boolean makePlacement(String player, int x, int y) {
-		if (0 <= x && x <= 2 && 0 <= y && y <= y) {
-			int stringIndex = getStringIndexForCoordinates(x, y);
-			String stringAtIndex = "" + gridString.charAt(stringIndex);
-			if (stringAtIndex.equals("X") || stringAtIndex.equals("O")) {
-				return false;
-			} else if (stringAtIndex.equals(" ") && (player.equals("X") || player.equals("O")) ) {
-				gridString = gridString.substring(0, stringIndex) + player + gridString.substring(stringIndex+1);
-				return true;
-			} else {
-				// Invalid String
-				return false;
-			}
-		} else {
-			// Invalid index
-			return false;			
+	public char getCell(int x, int y) {
+		return gridString.charAt(indexAt(x, y));
+	}
+	
+	public boolean isOccupied(int x, int y) {
+		return getCell(x, y) != ' ';
+	}
+	public boolean setCell(char c, int x, int y) {
+		if (! isOccupied(x, y)) {
+			int index = indexAt(x, y);
+			gridString = gridString.substring(0, index) + c + gridString.substring(index+1);
+			return true;
 		}
+		return false;
+	}
+	public String toString() {
+		String str = "";
+		for (int y = 0; y <= 2; y++) {
+			for (int x = 0; x <= 2; x++) {
+				str += " " + gridString.charAt(indexAt(x, y)) + " |";
+			}
+			str = str.substring(0,str.length()-2) + "\n-----------\n";
+		}
+		return str.substring(0, str.length()-12);
 	}
 	
 	/* 
@@ -40,127 +50,91 @@ public class TicTacToe {
 	 * ---------------------
 	 * (0,2) | (1,2) | (2,2)
 	 */
-	public int getStringIndexForCoordinates(int x, int y) {
+	public int indexAt(int x, int y) {
 		return x + 3 * y;
-		
 	}
+
+	/*
+	 * PART 2
+	 */
 	
-	public String hasWon() {
+	public boolean isFinished() {
+		if (hasWinner()) {
+			return true;
+		} else {
+			// If there is no winner and there are unoccupied positions on the board we're still not finished
+			for (int i = 0; i < gridString.length(); i++) {
+				if (gridString.charAt(i) == ' ') {
+					return false;
+				}
+			}
+			// Draw
+			return true;
+		}
+	}
+	public boolean isWinner(char c) {
+		String win = "" + c + c +c;
 		// Three in a row
 		String row1 = gridString.substring(0, 3);
 		String row2 = gridString.substring(3, 6);
 		String row3 = gridString.substring(6, 9);
-		if (row1.contains("XXX") || row2.contains("XXX") || row3.contains("XXX")) {
-			return "X";
-		} else if (row1.contains("OOO") || row2.contains("OOO") || row3.contains("OOO")) {
-			return "O";
+		if (row1.contains(win) || row2.contains(win) || row3.contains(win)) {
+			return true;
 		}
-		
 		// Three in a column
 		String col1 = "" + gridString.charAt(0) + gridString.charAt(3) + gridString.charAt(6);
 		String col2 = "" + gridString.charAt(1) + gridString.charAt(4) + gridString.charAt(7);
 		String col3 = "" + gridString.charAt(2) + gridString.charAt(5) + gridString.charAt(8);
-		if (col1.contains("XXX") || col2.contains("XXX") || col3.contains("XXX")) {
-			return "X";
-		} else if (col1.contains("OOO") || col2.contains("OOO") || col3.contains("OOO")) {
-			return "O";
+		if (col1.contains(win) || col2.contains(win) || col3.contains(win)) {
+			return true;
 		}
-		
 		// Three across
 		String across1 = "" + gridString.charAt(0) + gridString.charAt(4) + gridString.charAt(8);
 		String across2 = "" + gridString.charAt(2) + gridString.charAt(4) + gridString.charAt(6);
-		if (across1.contains("XXX") || across2.contains("XXX")) {
-			return "X";
-		} else if (across1.contains("OOO") || across2.contains("OOO")) {
-			return "O";
+		if (across1.contains(win) || across2.contains(win)) {
+			return true;
 		}
-		// Draw
-		for (int i = 0; i < gridString.length(); i++) {
-			if (gridString.charAt(i) == ' ') {
-				return null;
-			}
-		}
-		return "draw";
+		return false;
 	}
-	
-	public String toString() {
-		String str = "";
-		for (int y = 0; y <= 2; y++) {
-			for (int x = 0; x <= 2; x++) {
-				str += " " + gridString.charAt(getStringIndexForCoordinates(x, y)) + " |";
-			}
-			str = str.substring(0,str.length()-2) + "\n-----------\n";
-		}
-		return str.substring(0, str.length()-12);
+		
+	public boolean hasWinner() {
+		return isWinner('x') || isWinner('o');
 	}
-	
-	public String changePlayer(String player) {
-		if (player.equals("X")) {
-			return "O";
-		} else {
-			return "X";
+	public void play(int x, int y) {
+		if (setCell(player, x, y)) {
+			// Change player
+			player = player == 'x' ? 'o' : 'x';
 		}
 	}
 	
-	public int getXForInput(String input) {
-		if ("qaz".contains(input)) {
-			return 0;
-		} else if ("wsx".contains(input)) {
-			return 1;
-		} else if ("edc".contains(input)) {
-			return 2;
-		} else {
-			return -1;
-		}
-	}
-
-	public int getYForInput(String input) {
-		if ("qwe".contains(input)) {
-			return 0;
-		} else if ("asd".contains(input)) {
-			return 1;
-		} else if ("zxc".contains(input)) {
-			return 2;
-		} else {
-			return -1;
-		}
-	}
+	/*
+	 * PART 3
+	 */
 	
 	public void printInstructions() {
-		String str = "Use keys corresponding to board positions:\n q | w | e \n-----------\n a | s | d \n-----------\n z | x | c \n###########\n";
+		String str = "Enter x and y coordinate of next placement (separated by whitespace) corresponding to board positions:\n 0 0 | 1 0 | 2 0 \n-----------------\n 0 1 | 1 1 | 2 1 \n-----------------\n 0 2 | 1 2 | 2 2 \n###########\n";
 		System.out.println(str);
 	}
-	
-	public boolean play() {
-		Scanner scr = new Scanner(System.in);
-		String player = "X";
-		printInstructions();
-		while (hasWon() == null) {
-			// Read input from user
-			System.out.println(this);
-			System.out.println("Player " + player + " make a move:");
-			String input = "" + scr.next().charAt(0);
-			int x = getXForInput(input);
-			int y = getYForInput(input);
-			boolean validPlacement = makePlacement(player, x, y);
-			if (validPlacement && x != -1 && y != -1) {
-				player = changePlayer(player);
-			}
-			// Check win condition
-			String hasWonString = hasWon(); 
-			if (hasWonString == "X" || hasWonString == "Y" ) {
-				System.out.println(this);
-				System.out.println("Player " + hasWonString + " has won the game.");
-			} 
-			if (hasWonString == "draw") {
-				System.out.println("The game ended in a draw.");
-			}
-		}
-		return true;
-	}
-	
+		
 	public static void main(String[] args) {
 		TicTacToe ttt = new TicTacToe();
-		ttt.play();
+		Scanner scr = new Scanner(System.in);
+		ttt.printInstructions();
+		while (! ttt.isFinished()) {
+			System.out.println(ttt);
+			System.out.println("Player " + ttt.player + ", enter position of desired move:");
+			int x = scr.nextInt();
+			int y = scr.nextInt();
+			ttt.play(x, y);
+		}
+		System.out.println(ttt);
+		if (! ttt.hasWinner()) {
+			// The game ended in a draw
+			System.out.println("The game ended in a draw.");
+		} else if (ttt.isWinner('x')) {
+			System.out.println("Player x has won the game.");
+		} else if (ttt.isWinner('o')) {
+			System.out.println("Player o has won the game.");
+		} 
 	}
 }
